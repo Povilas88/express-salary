8a457cb5ee888ac5db06226d23b22c088e6415ef
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-08-26 14:53:30 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { pageHome } from './pages/pageHome.js';
+import { page404 } from './pages/page404.js';
+import { pageAbout } from './pages/pageAbout.js';
+
+const app = express();
+const port = 5019;
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    return res.send(pageHome())
+})
+
+app.get('/about', (req, res) => {
+    return res.send(pageAbout())
+})
+
+// app.get('/css/main.css', (req, res) => {
+//     return res.setHeader('Content-Type', 'text/css').send(`* {margin: 0;}`)
+// })
+
+app.get('*', (req, res) => {
+    return res.send(page404())
+})
+
+
+app.listen(port, () => {
+    console.log(`Server started: http://localhost:${port}`);
+});
---
+++ pages/pageAbout.js
@@ -0,0 +1,34 @@
+export function pageAbout() {
+    return `
+    <!DOCTYPE html>
+    <html lang = "en" >
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Salaries</title>
+    </head>
+    <body>
+    <header>
+        <img src="#" alt="Logo">
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+        </nav>
+    </header>
+
+    <main>
+        <h1>About project</h1>
+        <p>Project, where you can:</p>
+        <h2>User profile:</h2>
+        <ul>
+            <li>Register</li>
+            <li>See all registered users</li>
+            <li>Remove</li>
+            <li>Edit name</li>
+            <li>Edit surname</li>
+        </ul>
+    </main> 
+
+    </body>
+    </html>`;
+}
